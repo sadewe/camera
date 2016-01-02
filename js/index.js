62cc71d6a12d9bba28eae38690e2254266e718be
sarah weber <sarah weber>
2016-01-02 21:29:03 +0100

    upload files

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    startCamera();
+});
+    
+    function startCamera() {
+        document.addEventListener('deviceready', this.takePicture, false);
+    }
+
+    function takePicture(){
+        $(".button--submit").on("click", function(){
+            alert("click");
+            navigator.camera.getPicture(onSuccess, onFail, {
+                quality: 25,
+                destinationType: Camera.DestinationType.FILE_URI,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                saveToPhotoAlbum: true
+            });
+        });
+    }
+
+    function onSuccess(imageURI){
+        var image = document.getElementById('img');
+        image.style.visibility = "visible";
+        image.style.display = "block";
+        image.style.width = "2rem";
+        image.style.height = "2rem;";
+        image.src = imageURI;
+    }
+
+    function onFail(message){
+        alert("Failed because: " + message);
+    }
